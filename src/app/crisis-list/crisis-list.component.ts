e0156773c51b1e74fd861e1badaf32011e998b90
Swexato <denysalves@yahoo.com.br>
2024-04-12 14:18:57 -0300

    Início

---
+++ src/app/crisis-list/crisis-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crisis-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './crisis-list.component.html',
+  styleUrl: './crisis-list.component.scss'
+})
+
+export class CrisisListComponent {
+
+}
